ab9fff2f3440ae3802472c243a550dda048eff07
way2Arun <way2aru@yahoo.com>
2020-10-06 08:12:58 -0400

    LeetCode Challenge - leet code challenge  Insert into a Binary Search Tree

---
+++ src/trees/insertIntoBST.py
@@ -0,0 +1,84 @@
+"""
+Insert into a Binary Search Tree
+You are given the root node of a binary search tree (BST) and a value to insert into the tree. Return the root node of the BST after the insertion. It is guaranteed that the new value does not exist in the original BST.
+
+Notice that there may exist multiple valid ways for the insertion, as long as the tree remains a BST after insertion. You can return any of them.
+
+
+
+Example 1:
+
+
+Input: root = [4,2,7,1,3], val = 5
+Output: [4,2,7,1,3,5]
+Explanation: Another accepted tree is:
+
+Example 2:
+
+Input: root = [40,20,60,10,30,50,70], val = 25
+Output: [40,20,60,10,30,50,70,null,null,25]
+Example 3:
+
+Input: root = [4,2,7,1,3,null,null,null,null,null,null], val = 5
+Output: [4,2,7,1,3,5]
+
+
+Constraints:
+
+The number of nodes in the tree will be in the range [0, 104].
+-108 <= Node.val <= 108
+All the values Node.val are unique.
+-108 <= val <= 108
+It's guaranteed that val does not exist in the original BST.
+"""
+
+from src.trees.printBinaryTree import PrintBinaryTree
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def insertIntoBST(self, root: TreeNode, val: int) -> TreeNode:
+        # Solution 1 - 136 ms
+        """
+        if not root:
+            return TreeNode(val)
+        if val > root.val:
+            root.right = self.insertIntoBST(root.right, val)
+        else:
+            root.left = self.insertIntoBST(root.left, val)
+
+        return root
+        """
+        # Solution 2 - 112 ms
+        if not root:
+            return TreeNode(val)
+
+        if root.val < val:
+            root.right = self.insertIntoBST(root.right, val)
+        else:
+            root.left = self.insertIntoBST(root.left, val)
+
+        return root
+
+
+# Main Call
+tree_root = TreeNode(4)
+tree_root.left = TreeNode(2)
+tree_root.left.left = TreeNode(1)
+tree_root.left.right = TreeNode(3)
+tree_root.right = TreeNode(7)
+
+solution = Solution()
+val = 5
+tree_node = solution.insertIntoBST(tree_root, val)
+# Lets print the binary tree
+print_tree = PrintBinaryTree()
+response = print_tree.print_tree(tree_node)
+print(response)
