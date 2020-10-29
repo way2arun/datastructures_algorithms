837ec8ea7bc6497473da6d7e48c0f8aee827022f
way2Arun <way2aru@yahoo.com>
2020-10-29 11:39:48 -0400

    LeetCode Challenge - leet code challenge  Maximize Distance to Closest Person

---
+++ src/arrays/maxDistToClosest.py
@@ -0,0 +1,85 @@
+"""
+Maximize Distance to Closest Person
+You are given an array representing a row of seats where seats[i] = 1 represents a person sitting in the ith seat, and seats[i] = 0 represents that the ith seat is empty (0-indexed).
+
+There is at least one empty seat, and at least one person sitting.
+
+Alex wants to sit in the seat such that the distance between him and the closest person to him is maximized.
+
+Return that maximum distance to the closest person.
+
+
+
+Example 1:
+
+
+Input: seats = [1,0,0,0,1,0,1]
+Output: 2
+Explanation:
+If Alex sits in the second open seat (i.e. seats[2]), then the closest person has distance 2.
+If Alex sits in any other open seat, the closest person has distance 1.
+Thus, the maximum distance to the closest person is 2.
+Example 2:
+
+Input: seats = [1,0,0,0]
+Output: 3
+Explanation:
+If Alex sits in the last seat (i.e. seats[3]), the closest person is 3 seats away.
+This is the maximum distance possible, so the answer is 3.
+Example 3:
+
+Input: seats = [0,1]
+Output: 1
+
+
+Constraints:
+
+2 <= seats.length <= 2 * 104
+seats[i] is 0 or 1.
+At least one seat is empty.
+At least one seat is occupied.
+
+"""
+from typing import List
+
+
+class Solution:
+    def maxDistToClosest(self, seats: List[int]) -> int:
+        # Solution 1 - 124 ms
+        """
+        ans = 0
+        p1, p2 = -1, 0
+        while p2 < len(seats):
+            if seats[p2]:
+                ans = max(ans, (p2 - p1) // 2) if p1 != -1 else p2
+                p1 = p2
+            p2 += 1
+        if not seats[p2 - 1]:
+            ans = max(ans, p2 - p1 - 1)
+        return ans
+        """
+        # Solution 2 - 108 ms
+        prev = None
+        diff = 0
+        result = -1
+
+        n = len(seats)
+        for i in range(n):
+            if seats[i]:
+                if prev is None:
+                    prev = i
+                    left = i
+                elif i - prev > diff:
+                    diff = i - prev
+                    result = diff // 2
+                prev = i
+
+        right = n - prev - 1
+
+        return max(left, right, result)
+
+
+# Main Call
+seats = [1, 0, 0, 0]
+solution = Solution()
+print(solution.maxDistToClosest(seats))
