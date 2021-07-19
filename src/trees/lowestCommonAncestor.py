78d77215dd18a2ed42f1a66c0343cd5d052c540b
way2Arun <way2aru@yahoo.com>
2021-07-19 10:22:32 -0400

    LeetCode Challenge - leet code challenge  Lowest Common Ancestor of a Binary Search Tree

---
+++ src/trees/lowestCommonAncestor.py
@@ -0,0 +1,90 @@
+"""
+Lowest Common Ancestor of a Binary Search Tree
+Given a binary search tree (BST), find the lowest common ancestor (LCA) of two given nodes in the BST.
+
+According to the definition of LCA on Wikipedia: “The lowest common ancestor is defined between two nodes p and q as the lowest node in T that has both p and q as descendants (where we allow a node to be a descendant of itself).”
+
+
+
+Example 1:
+
+
+Input: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 8
+Output: 6
+Explanation: The LCA of nodes 2 and 8 is 6.
+Example 2:
+
+
+Input: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 4
+Output: 2
+Explanation: The LCA of nodes 2 and 4 is 2, since a node can be a descendant of itself according to the LCA definition.
+Example 3:
+
+Input: root = [2,1], p = 2, q = 1
+Output: 2
+
+
+Constraints:
+
+The number of nodes in the tree is in the range [2, 105].
+-109 <= Node.val <= 109
+All Node.val are unique.
+p != q
+p and q will exist in the BST.
+"""
+from src.trees.printBinaryTree import PrintBinaryTree
+
+# Definition for a binary tree node.
+class TreeNode:
+     def __init__(self, x):
+         self.val = x
+         self.left = None
+         self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        # Solution 1 - 72 ms
+        """
+        small = min(p.val, q.val)
+        large = max(p.val, q.val)
+        while root:
+            if root.val > large:  # p, q belong to the left subtree
+                root = root.left
+            elif root.val < small:  # p, q belong to the right subtree
+                root = root.right
+            else:  # Now, small <= root.val <= large -> This is the LCA between p and q
+                return root
+        return None
+        """
+        # Solution 2 - 52 ms
+        def helper(root, p, q):
+
+            if p.val < root.val and q.val < root.val:
+                return self.lowestCommonAncestor(root.left, p, q)
+            if p.val > root.val and q.val > root.val:
+                return self.lowestCommonAncestor(root.right, p, q)
+            else:
+                return root
+
+        return helper(root, p, q)
+
+
+# Main Call
+root = TreeNode(6)
+root.left = TreeNode(2)
+root.left.left = TreeNode(0)
+root.left.right = TreeNode(4)
+root.left.right.left = TreeNode(3)
+root.left.right.right = TreeNode(5)
+root.right = TreeNode(8)
+root.right.left = TreeNode(7)
+root.right.right = TreeNode(9)
+
+p = TreeNode(2)
+q = TreeNode(8)
+
+solution = Solution()
+node = solution.lowestCommonAncestor(root, p, q)
+print_tree = PrintBinaryTree()
+response = print_tree.print_tree(node)
+print(response)
